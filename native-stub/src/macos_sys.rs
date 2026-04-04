59298bb391c75a8154af2b8d0bcc03c59b9c2d08
R <rqou@berkeley.edu>
2026-04-04 06:19:48 +0100

    Port initial iokit watch setup code
    It doesn't work yet, because we need the Mach message dispatch

--- native-stub/src/macos_sys.rs
+++ native-stub/src/macos_sys.rs
@@ -1,12 +1,36 @@
-use libc::mach_port_t;
+use core_foundation::dictionary::{CFDictionaryRef, CFMutableDictionaryRef};
+use libc::{kern_return_t, mach_port_t};
 
 #[repr(C)]
 pub struct IONotificationPort {
     _private: [u8; 0],
 }
 
+#[allow(non_camel_case_types)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Default, Hash)]
+#[repr(transparent)]
+pub struct io_object_t(pub mach_port_t);
+impl From<io_object_t> for mach_port_t {
+    fn from(value: io_object_t) -> Self {
+        value.0
+    }
+}
+
 unsafe extern "C" {
     pub fn IONotificationPortCreate(main_port: mach_port_t) -> *mut IONotificationPort;
     pub fn IONotificationPortDestroy(notify: *mut IONotificationPort);
     pub fn IONotificationPortGetMachPort(notify: *mut IONotificationPort) -> mach_port_t;
+
+    pub fn IOServiceMatching(name: *const u8) -> CFMutableDictionaryRef;
+    pub fn IOServiceAddMatchingNotification(
+        notify: *mut IONotificationPort,
+        notification_type: *const u8,
+        matching: CFDictionaryRef,
+        callback: extern "C" fn(*const (), io_object_t),
+        refcon: *const (),
+        notification: *mut io_object_t,
+    ) -> kern_return_t;
+
+    pub fn IOIteratorNext(iterator: io_object_t) -> io_object_t;
+    pub fn IOObjectRelease(object: io_object_t) -> kern_return_t;
 }
--- native-stub/src/main.rs
+++ native-stub/src/main.rs
@@ -1,5 +1,9 @@
-use std::{io, ptr};
+use std::cell::RefCell;
+use std::convert::Infallible;
+use std::io;
+use std::ptr;
 
+use core_foundation::base::CFRetain;
 use kqueue_sys::*;
 
 mod macos_sys;
@@ -10,11 +14,19 @@ use macos_sys::*;
 /// Main struct holding all of the state for our operations
 pub struct USBStubEngine {
     kqueue: i32,
-    kevents_buf: Vec<kevent>,
+    kevents_buf: RefCell<Vec<kevent>>,
+    // The following only need to be held on to, we don't touch them
     _io_notification_port: *mut IONotificationPort,
+    _plug_notifications: io_object_t,
+    _unplug_notifications: io_object_t,
 }
 impl USBStubEngine {
-    pub fn init() -> Self {
+    // XXX: Work around rustfmt not wanting to work otherwise??
+    fn init_real(
+        mut this: pin_init::PinUninit<'_, Self>,
+    ) -> pin_init::InitResult<'_, Self, Infallible> {
+        let v = this.get_mut().as_mut_ptr();
+
         // Create kqueue fd
         let kq = unsafe { kqueue() };
 
@@ -63,23 +75,71 @@ impl USBStubEngine {
         // Prepare buffer for reading events
         let kevents_buf = Vec::with_capacity(all_kevents.len());
 
-        Self {
-            kqueue: kq,
-            kevents_buf,
-            _io_notification_port: io_notif_port,
+        // Register IOKit notifications
+        let matching = unsafe { IOServiceMatching(b"IOUSBDevice\0".as_ptr()) };
+        // SAFETY: We add two matching notifications, so bump refcount by 1
+        unsafe { CFRetain(matching as *const _) };
+
+        let mut plug_notifications = io_object_t(0);
+        let ret = unsafe {
+            IOServiceAddMatchingNotification(
+                io_notif_port,
+                b"IOServiceFirstMatch\0".as_ptr(),
+                matching,
+                Self::iokit_plug_cb,
+                v as *const (),
+                &mut plug_notifications,
+            )
+        };
+        assert_eq!(ret, 0, "failed to watch USB hotplug notifications");
+
+        let mut unplug_notifications = io_object_t(0);
+        let ret = unsafe {
+            IOServiceAddMatchingNotification(
+                io_notif_port,
+                b"IOServiceTerminate\0".as_ptr(),
+                matching,
+                Self::iokit_unplug_cb,
+                v as *const (),
+                &mut unplug_notifications,
+            )
+        };
+        assert_eq!(ret, 0, "failed to watch USB hotplug unplug notifications");
+
+        // SAFETY: Make sure we set everything
+        unsafe {
+            (*v).kqueue = kq;
+            // SAFETY: Don't drop uninit buf
+            // (but others are okay, no drop impl)
+            ptr::addr_of_mut!((*v).kevents_buf).write(RefCell::new(kevents_buf));
+            (*v)._io_notification_port = io_notif_port;
+            (*v)._plug_notifications = plug_notifications;
+            (*v)._unplug_notifications = unplug_notifications;
         }
+
+        // Invoke iteration once to capture everything pre-existing
+        Self::iokit_plug_cb(v as *const (), plug_notifications);
+        Self::iokit_unplug_cb(v as *const (), unplug_notifications);
+
+        // SAFETY: Make sure we set everything
+        unsafe { Ok(this.init_ok()) }
+    }
+
+    pub fn init() -> impl pin_init::Init<Self, Infallible> {
+        pin_init::init_from_closure(Self::init_real)
     }
 
     /// Run one loop. Returns true if we should continue
-    pub fn run_loop(&mut self) -> bool {
+    pub fn run_loop(&self) -> bool {
         // Poll for events
+        let mut kevents_buf = self.kevents_buf.borrow_mut();
         unsafe {
             let nevents = kqueue_sys::kevent(
                 self.kqueue,
                 ptr::null(),
                 0,
-                self.kevents_buf.as_mut_ptr(),
-                self.kevents_buf.capacity() as i32,
+                kevents_buf.as_mut_ptr(),
+                kevents_buf.capacity() as i32,
                 ptr::null(),
             );
 
@@ -88,10 +148,10 @@ impl USBStubEngine {
             }
 
             // SAFETY: Set length to actual number of events
-            self.kevents_buf.set_len(nevents as usize);
+            kevents_buf.set_len(nevents as usize);
         };
 
-        for kevent in self.kevents_buf.iter() {
+        for kevent in kevents_buf.iter() {
             if kevent.ident == 0 && kevent.filter == EventFilter::EVFILT_READ {
                 let msg = stdio_unix::read_stdin_msg();
                 if let Err(e) = &msg
@@ -112,10 +172,42 @@ impl USBStubEngine {
 
         true
     }
+
+    extern "C" fn iokit_plug_cb(self_: *const (), iterator: io_object_t) {
+        dbg!("plug!");
+        // SAFETY: We passed in self as the arg
+        let self_ = unsafe { &*(self_ as *const Self) };
+        dbg!(self_.kqueue);
+
+        let mut item;
+        loop {
+            item = unsafe { IOIteratorNext(iterator) };
+            if item.0 == 0 {
+                break;
+            }
+        }
+    }
+
+    extern "C" fn iokit_unplug_cb(self_: *const (), iterator: io_object_t) {
+        dbg!("unplug!");
+        // SAFETY: We passed in self as the arg
+        let self_ = unsafe { &*(self_ as *const Self) };
+        dbg!(self_.kqueue);
+
+        let mut item;
+        loop {
+            item = unsafe { IOIteratorNext(iterator) };
+            if item.0 == 0 {
+                break;
+            }
+        }
+    }
 }
 impl Drop for USBStubEngine {
     fn drop(&mut self) {
         unsafe {
+            IOObjectRelease(self._plug_notifications);
+            IOObjectRelease(self._unplug_notifications);
             IONotificationPortDestroy(self._io_notification_port);
             libc::close(self.kqueue);
         }
@@ -125,7 +217,12 @@ impl Drop for USBStubEngine {
 fn main() {
     eprintln!("Hello, world!");
 
-    let mut state = USBStubEngine::init();
+    pin_init::init_stack!(state = USBStubEngine::init());
+    let state = state.unwrap();
+    // SAFETY: We need to *immediately* get rid of &mut,
+    // because OS callbacks etc hold a reference to the engine object.
+    // This is probably *still* incorrect, but dunno how to fix.
+    let state = state.as_ref();
     while state.run_loop() {}
 
     eprintln!("zzz");
