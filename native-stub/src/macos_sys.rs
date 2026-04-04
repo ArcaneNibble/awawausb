1a9cb75d559605f67de402f360787112fb71c20a
R <rqou@berkeley.edu>
2026-04-04 07:13:42 +0100

    Add USB interface struct back

--- native-stub/src/macos_sys.rs
+++ native-stub/src/macos_sys.rs
@@ -5,7 +5,7 @@ use core_foundation::{
     dictionary::{CFDictionaryRef, CFMutableDictionaryRef},
     string::CFStringRef,
 };
-use libc::{kern_return_t, mach_port_t};
+use libc::{kern_return_t, mach_port_t, size_t};
 
 #[repr(C, align(8))]
 pub struct OpaqueMachMessage {
@@ -146,6 +146,25 @@ pub struct IOUSBFindInterfaceRequest {
 #[repr(C)]
 #[allow(non_snake_case)]
 #[derive(Debug)]
+pub struct IOUSBIsocFrame {
+    pub frStatus: kern_return_t,
+    pub frReqCount: u16,
+    pub frActCount: u16,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[derive(Debug)]
+pub struct IOUSBLowLatencyIsocFrame {
+    pub frStatus: kern_return_t,
+    pub frReqCount: u16,
+    pub frActCount: u16,
+    pub frTimeStamp: u64,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[derive(Debug)]
 pub struct IUnknown {
     _reserved: *const (),
     pub QueryInterface: unsafe extern "C" fn(*const (), CFUUIDBytes, *mut *const ()) -> i32,
@@ -219,6 +238,162 @@ pub struct IOUSBDeviceStruct320 {
     pub GetExtraPowerAllocated: unsafe extern "C" fn(*const (), u32, *mut u32) -> kern_return_t,
 }
 
+#[repr(C)]
+#[allow(non_snake_case)]
+#[derive(Debug)]
+pub struct IOUSBInterfaceStruct197 {
+    pub IUnknown: IUnknown,
+    pub CreateInterfaceAsyncEventSource:
+        unsafe extern "C" fn(*const (), *mut *const ()) -> kern_return_t,
+    pub GetInterfaceAsyncEventSource: unsafe extern "C" fn(*const ()) -> *const (),
+    pub CreateInterfaceAsyncPort:
+        unsafe extern "C" fn(*const (), *mut mach_port_t) -> kern_return_t,
+    pub GetInterfaceAsyncPort: unsafe extern "C" fn(*const ()) -> mach_port_t,
+    pub USBInterfaceOpen: unsafe extern "C" fn(*const ()) -> kern_return_t,
+    pub USBInterfaceClose: unsafe extern "C" fn(*const ()) -> kern_return_t,
+    pub GetInterfaceClass: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetInterfaceSubClass: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetInterfaceProtocol: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetDeviceVendor: unsafe extern "C" fn(*const (), *mut u16) -> kern_return_t,
+    pub GetDeviceProduct: unsafe extern "C" fn(*const (), *mut u16) -> kern_return_t,
+    pub GetDeviceReleaseNumber: unsafe extern "C" fn(*const (), *mut u16) -> kern_return_t,
+    pub GetConfigurationValue: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetInterfaceNumber: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetAlternateSetting: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetNumEndpoints: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub GetLocationID: unsafe extern "C" fn(*const (), *mut u32) -> kern_return_t,
+    pub GetDevice: unsafe extern "C" fn(*const (), *mut io_object_t) -> kern_return_t,
+    pub SetAlternateInterface: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub GetBusFrameNumber: unsafe extern "C" fn(*const (), *mut u64, *mut u64) -> kern_return_t,
+    pub ControlRequest: unsafe extern "C" fn(*const (), u8, *mut IOUSBDevRequest) -> kern_return_t,
+    pub ControlRequestAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const IOUSBDevRequest,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub GetPipeProperties: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *mut u8,
+        *mut u8,
+        *mut u8,
+        *mut u16,
+        *mut u8,
+    ) -> kern_return_t,
+    pub GetPipeStatus: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub AbortPipe: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub ResetPipe: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub ClearPipeStall: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub ReadPipe: unsafe extern "C" fn(*const (), u8, *mut (), *mut u32) -> kern_return_t,
+    pub WritePipe: unsafe extern "C" fn(*const (), u8, *const (), u32) -> kern_return_t,
+    pub ReadPipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *mut (),
+        u32,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub WritePipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const (),
+        u32,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub ReadIsochPipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *mut (),
+        u64,
+        u32,
+        *mut IOUSBIsocFrame,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub WriteIsochPipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const (),
+        u64,
+        u32,
+        *mut IOUSBIsocFrame,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub ControlRequestTO:
+        unsafe extern "C" fn(*const (), u8, *mut IOUSBDevRequestTO) -> kern_return_t,
+    pub ControlRequestAsyncTO: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const IOUSBDevRequestTO,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub ReadPipeTO:
+        unsafe extern "C" fn(*const (), u8, *mut (), *mut u32, u32, u32) -> kern_return_t,
+    pub WritePipeTO: unsafe extern "C" fn(*const (), u8, *const (), u32, u32, u32) -> kern_return_t,
+    pub ReadPipeAsyncTO: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *mut (),
+        u32,
+        u32,
+        u32,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub WritePipeAsyncTO: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const (),
+        u32,
+        u32,
+        u32,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub USBInterfaceGetStringIndex: unsafe extern "C" fn(*const (), *mut u8) -> kern_return_t,
+    pub USBInterfaceOpenSeize: unsafe extern "C" fn(*const ()) -> kern_return_t,
+    pub ClearPipeStallBothEnds: unsafe extern "C" fn(*const (), u8) -> kern_return_t,
+    pub SetPipePolicy: unsafe extern "C" fn(*const (), u8, u16, u8) -> kern_return_t,
+    pub GetBandwidthAvailable: unsafe extern "C" fn(*const (), *mut u32) -> kern_return_t,
+    pub GetEndpointProperties:
+        unsafe extern "C" fn(*const (), u8, u8, u8, *mut u8, *mut u16, *mut u8) -> kern_return_t,
+    pub LowLatencyReadIsochPipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *mut (),
+        u64,
+        u32,
+        u32,
+        *mut IOUSBLowLatencyIsocFrame,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub LowLatencyWriteIsochPipeAsync: unsafe extern "C" fn(
+        *const (),
+        u8,
+        *const (),
+        u64,
+        u32,
+        u32,
+        *mut IOUSBLowLatencyIsocFrame,
+        extern "C" fn(*const (), kern_return_t, *const ()),
+        *const (),
+    ) -> kern_return_t,
+    pub LowLatencyCreateBuffer:
+        unsafe extern "C" fn(*const (), *mut *mut (), size_t, u32) -> kern_return_t,
+    pub LowLatencyDestroyBuffer: unsafe extern "C" fn(*const (), *mut ()) -> kern_return_t,
+    pub GetBusMicroFrameNumber:
+        unsafe extern "C" fn(*const (), *mut u64, *mut u64) -> kern_return_t,
+    pub GetFrameListTime: unsafe extern "C" fn(*const (), *mut u32) -> kern_return_t,
+    pub GetIOUSBLibVersion: unsafe extern "C" fn(*const (), *mut u32, *mut u32) -> kern_return_t,
+}
+
 #[allow(non_snake_case)]
 pub fn kIOCFPlugInInterfaceID() -> *const CFUUID {
     unsafe {
@@ -269,9 +444,39 @@ pub fn kIOUSBDeviceUserClientTypeID() -> *const CFUUID {
     }
 }
 
+#[allow(non_snake_case)]
+pub fn kIOUSBInterfaceUserClientTypeID() -> *const CFUUID {
+    unsafe {
+        CFUUIDGetConstantUUIDWithBytes(
+            ptr::null(),
+            0x2d,
+            0x97,
+            0x86,
+            0xc6,
+            0x9e,
+            0xf3,
+            0x11,
+            0xD4,
+            0xad,
+            0x51,
+            0x00,
+            0x0a,
+            0x27,
+            0x05,
+            0x28,
+            0x61,
+        )
+    }
+}
+
 #[allow(non_upper_case_globals)]
 pub const kIOUSBDeviceInterfaceID320: CFUUIDBytes = CFUUIDBytes::new([
     0x01, 0xA2, 0xD0, 0xE9, 0x42, 0xF6, 0x4A, 0x87, 0x8B, 0x8B, 0x77, 0x05, 0x7C, 0x8C, 0xE0, 0xCE,
+]);
+
+#[allow(non_upper_case_globals)]
+pub const kIOUSBInterfaceInterfaceID197: CFUUIDBytes = CFUUIDBytes::new([
+    0xC6, 0x3D, 0x3C, 0x92, 0x08, 0x84, 0x11, 0xD7, 0x96, 0x92, 0x00, 0x03, 0x93, 0x3E, 0x3E, 0x3E,
 ]);
 
 unsafe extern "C" {
--- native-stub/src/macos_wrap.rs
+++ native-stub/src/macos_wrap.rs
@@ -14,15 +14,13 @@ impl IOUSBDeviceStruct {
             // TODO: libusb has a loop workaround thing here. Do we actually need that?
             let mut iokit_plugin = ptr::null();
             let mut score = 0;
-            let ret = unsafe {
-                IOCreatePlugInInterfaceForService(
-                    obj,
-                    kIOUSBDeviceUserClientTypeID(),
-                    kIOCFPlugInInterfaceID(),
-                    &mut iokit_plugin,
-                    &mut score,
-                )
-            };
+            let ret = IOCreatePlugInInterfaceForService(
+                obj,
+                kIOUSBDeviceUserClientTypeID(),
+                kIOCFPlugInInterfaceID(),
+                &mut iokit_plugin,
+                &mut score,
+            );
             assert_eq!(
                 ret, 0,
                 "IOCreatePlugInInterfaceForService failed! 0x{:08x}",
@@ -41,6 +39,7 @@ impl IOUSBDeviceStruct {
 
             // Don't need the plugin interface anymore
             ((*plugin_iunk).Release)(iokit_plugin);
+            IOObjectRelease(obj);
 
             Self(device as *mut *const IOUSBDeviceStruct320)
         }
@@ -54,9 +53,95 @@ impl IOUSBDeviceStruct {
         };
         assert_eq!(ret, 0);
         println!("{:08x} {:08x}", lib_ver, fam_ver);
+
+        // iterator
+        let iter_ifaces = IOUSBFindInterfaceRequest {
+            bInterfaceClass: 0xffff,
+            bInterfaceSubClass: 0xffff,
+            bInterfaceProtocol: 0xffff,
+            bAlternateSetting: 0xffff,
+        };
+        let mut iface_iter_obj = io_object_t(0);
+        let ret = unsafe {
+            ((**self.0).CreateInterfaceIterator)(
+                self.0 as *const (),
+                &iter_ifaces,
+                &mut iface_iter_obj,
+            )
+        };
+        assert_eq!(ret, 0);
+        println!("iface iterator {:?}", iface_iter_obj);
+
+        let mut iface_iokit;
+        loop {
+            iface_iokit = unsafe { IOIteratorNext(iface_iter_obj) };
+            if iface_iokit.0 == 0 {
+                break;
+            }
+
+            let iface = unsafe { IOUSBInterfaceStruct::new(iface_iokit) };
+
+            let mut lib_ver = 0;
+            let mut fam_ver = 0;
+            let ret = unsafe {
+                ((**iface.0).GetIOUSBLibVersion)(iface.0 as *const (), &mut lib_ver, &mut fam_ver)
+            };
+            assert_eq!(ret, 0);
+            println!("{:08x} {:08x}", lib_ver, fam_ver);
+        }
     }
 }
 impl Drop for IOUSBDeviceStruct {
+    fn drop(&mut self) {
+        unsafe {
+            ((**self.0).IUnknown.Release)(self.0 as *const ());
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct IOUSBInterfaceStruct(*mut *const IOUSBInterfaceStruct197);
+impl IOUSBInterfaceStruct {
+    /// Turns an IOKit io_object_t into a USB interface interface
+    ///
+    /// Takes ownership of and *releases* the object
+    pub unsafe fn new(obj: io_object_t) -> Self {
+        unsafe {
+            // Get IOKit plugin interface
+            let mut iokit_plugin = ptr::null();
+            let mut score = 0;
+            let ret = IOCreatePlugInInterfaceForService(
+                obj,
+                kIOUSBInterfaceUserClientTypeID(),
+                kIOCFPlugInInterfaceID(),
+                &mut iokit_plugin,
+                &mut score,
+            );
+            assert_eq!(
+                ret, 0,
+                "IOCreatePlugInInterfaceForService failed! 0x{:08x}",
+                ret
+            );
+
+            let plugin_iunk = *(iokit_plugin as *const *const IUnknown);
+            let mut device = ptr::null();
+            let ret = ((*plugin_iunk).QueryInterface)(
+                iokit_plugin,
+                kIOUSBInterfaceInterfaceID197,
+                &mut device,
+            );
+            assert!(ret >= 0, "QueryInterface failed!");
+            assert!(!device.is_null(), "QueryInterface failed!");
+
+            // Don't need the plugin interface anymore
+            ((*plugin_iunk).Release)(iokit_plugin);
+            IOObjectRelease(obj);
+
+            Self(device as *mut *const IOUSBInterfaceStruct197)
+        }
+    }
+}
+impl Drop for IOUSBInterfaceStruct {
     fn drop(&mut self) {
         unsafe {
             ((**self.0).IUnknown.Release)(self.0 as *const ());
