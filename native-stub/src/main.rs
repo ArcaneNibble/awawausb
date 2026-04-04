7b54a0d1659dda19c485f9b6f5645b4e5e3b2e3e
R <rqou@berkeley.edu>
2026-04-04 05:30:02 +0100

    reimplement echo, without nonblocking stdin

--- native-stub/src/main.rs
+++ native-stub/src/main.rs
@@ -1,3 +1,75 @@
+use std::{io, ptr};
+
+use kqueue_sys::*;
+
+mod stdio_unix;
+
 fn main() {
-    println!("Hello, world!");
+    eprintln!("Hello, world!");
+
+    let kq = unsafe { kqueue() };
+
+    // Set up a kevent for stdin
+    let kevent_stdin = kevent::new(
+        0,
+        EventFilter::EVFILT_READ,
+        EventFlag::EV_ADD,
+        FilterFlag::empty(),
+    );
+
+    let all_kevents = [kevent_stdin];
+    if unsafe {
+        kqueue_sys::kevent(
+            kq,
+            all_kevents.as_ptr(),
+            all_kevents.len() as i32,
+            ptr::null_mut(),
+            0,
+            ptr::null(),
+        )
+    } < 0
+    {
+        panic!("kevent add failed: {:?}", std::io::Error::last_os_error());
+    }
+
+    let mut kevents_out_buf = Vec::with_capacity(all_kevents.len());
+
+    'outer: loop {
+        unsafe {
+            let nevents = kqueue_sys::kevent(
+                kq,
+                ptr::null(),
+                0,
+                kevents_out_buf.as_mut_ptr(),
+                kevents_out_buf.capacity() as i32,
+                ptr::null(),
+            );
+
+            if nevents < 0 {
+                panic!("kevent poll failed: {:?}", std::io::Error::last_os_error());
+            }
+
+            // SAFETY: Set length to actual number of events
+            kevents_out_buf.set_len(nevents as usize);
+        };
+
+        for kevent in kevents_out_buf.iter() {
+            if kevent.ident == 0 && kevent.filter == EventFilter::EVFILT_READ {
+                let msg = stdio_unix::read_stdin_msg();
+                if let Err(e) = &msg
+                    && e.kind() == io::ErrorKind::UnexpectedEof
+                {
+                    eprintln!("Goodbye!");
+                    break 'outer;
+                }
+                let msg = msg.expect("failed to read stdin");
+                dbg!(&msg);
+                stdio_unix::write_stdout_msg(&msg).expect("failed to write stdout");
+            } else {
+                dbg!(kevent);
+            }
+        }
+    }
+
+    eprintln!("zzz");
 }
---
+++ native-stub/src/stdio_unix.rs
@@ -0,0 +1,70 @@
+//! Unix-specific handling of stdio, in order to intentionally bypass Rust checks
+//!
+//! It is not clear the extent to which this is necessary,
+//! but our IO is performed totally unbuffered. Writing is also performed using iovec.
+//!
+//! In the future, Windows will need to bypass Rust in order to handle binary data (not UTF-8).
+
+use std::io;
+
+/// Reads a message from stdin
+pub fn read_stdin_msg() -> io::Result<Vec<u8>> {
+    // Read 4-byte length
+    let mut msglen = [0u8; 4];
+    let nbytes = unsafe { libc::read(0, msglen.as_mut_ptr() as *mut _, 4) };
+    if nbytes < 0 {
+        return Err(io::Error::last_os_error());
+    }
+    if nbytes < 4 {
+        return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
+    }
+    let msglen = u32::from_ne_bytes(msglen) as usize;
+
+    // Read actual message
+    let mut buf: Vec<u8> = Vec::with_capacity(msglen);
+    let nbytes = unsafe { libc::read(0, buf.as_mut_ptr() as *mut _, msglen) };
+    if nbytes < 0 {
+        return Err(io::Error::last_os_error());
+    }
+    if (nbytes as usize) < msglen {
+        return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
+    }
+    unsafe {
+        buf.set_len(msglen);
+    }
+
+    Ok(buf)
+}
+
+/// Writes a message to stdout
+pub fn write_stdout_msg(data: &[u8]) -> io::Result<()> {
+    let len = data.len();
+    if len > 1 * 1024 * 1024 {
+        panic!("message too long to send!");
+    }
+
+    let len = u32::to_ne_bytes(len as u32);
+    let iov = [
+        libc::iovec {
+            iov_base: len.as_ptr() as *mut _,
+            iov_len: 4,
+        },
+        libc::iovec {
+            iov_base: data.as_ptr() as *mut _,
+            iov_len: data.len(),
+        },
+    ];
+
+    let wbytes = unsafe { libc::writev(1, iov.as_ptr(), 2) };
+    if wbytes < 0 {
+        return Err(io::Error::last_os_error());
+    }
+    if (wbytes as usize) != data.len() + 4 {
+        return Err(io::Error::new(
+            io::ErrorKind::UnexpectedEof,
+            "stdout write truncated",
+        ));
+    }
+
+    Ok(())
+}
